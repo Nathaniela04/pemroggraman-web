0eeed5b79f3aec64e59e9db7e8e06b3964957f9b
Nathaniela <nielaisya@gmail.com>
2024-05-13 07:21:24 +0700

    first commit

---
+++ profil.js
@@ -0,0 +1,60 @@
+const txtElement = ['Nathaniela isya'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = ' ';
+let Words = ' ';
+
+(function ketik(){
+    if(count == txtElement.length){
+        count=0;
+    }
+    currentTxt = txtElement[count];
+
+    Words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek').textContent = Words;
+    if(Words.length==currentTxt.length){
+        count++;
+        txtIndex=0;
+    }
+    setTimeout(ketik, 500);
+}());
+
+const text = document.querySelector(".auto-type");
+
+const textLoad = () => {
+    const words = ["Portofolio", "Website", "Profile"];
+    let index = 0;
+    let wordIndex = 0;
+    let currentWord = "";
+
+    const typeWord = () => {
+        if (wordIndex < words[index].length) {
+            currentWord += words[index].charAt(wordIndex);
+            text.textContent = currentWord;
+            wordIndex++;
+            setTimeout(typeWord, 150); // Atur penundaan di sini (misalnya, 150 milidetik)
+        } else {
+            setTimeout(eraseWord, 1000); // Atur penundaan sebelum menghapus kata
+        }
+    };
+
+    const eraseWord = () => {
+        if (currentWord.length > 0) {
+            currentWord = currentWord.slice(0, -1);
+            text.textContent = currentWord;
+            setTimeout(eraseWord, 100); // Atur penundaan saat menghapus huruf (misalnya, 100 milidetik)
+        } else {
+            wordIndex = 0;
+            index = (index + 1) % words.length;
+            setTimeout(typeWord, 500); // Atur penundaan sebelum mengetik kata berikutnya (misalnya, 500 milidetik)
+        }
+    };
+
+    // Mulai proses
+    typeWord();
+};
+
+textLoad();
+
+
+
